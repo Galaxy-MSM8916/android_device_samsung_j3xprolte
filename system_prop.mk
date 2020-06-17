2b8e97735b01d8fd4f692b131d0a27acd188ee4a
zqm64 <zqm64@outlook.com>
2020-06-17 21:36:02 +0800

    j3xprolte: system_prop: Fix indents
    Change-Id: I7fb5dfce4ecf5ef0cb55fada2ab3b5cbb1152a50

--- system_prop.mk
+++ system_prop.mk
@@ -16,19 +16,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # CABL
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.qualcomm.cabl=2
+	ro.qualcomm.cabl=2
 
 # RAM
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.config.low_ram=false
+	ro.config.low_ram=false
 
 # RIL
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-        telephony.lteOnCdmaDevice=0 \
-        persist.radio.tdscdma_present=1 \
-        ro.telephony.default_network=9 \
-        ro.multisim.simslotcount=2 \
-        ro.telephony.ril.config=simactivation \
-        persist.radio.multisim.config=dsds \
-        rild.libpath2=/system/lib/libsec-ril-dsds.so \
-        ro.multisim.audio_follow_default_sim=false
+	telephony.lteOnCdmaDevice=0 \
+	persist.radio.tdscdma_present=1 \
+	ro.telephony.default_network=9 \
+	ro.multisim.simslotcount=2 \
+	ro.telephony.ril.config=simactivation \
+	persist.radio.multisim.config=dsds \
+	rild.libpath2=/system/lib/libsec-ril-dsds.so \
+	ro.multisim.audio_follow_default_sim=false
